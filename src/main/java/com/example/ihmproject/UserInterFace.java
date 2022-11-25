c439ab032a5d9b54c3f3822df8f0dcecb5b7f6c8
ZINE EDDINE <119061162+ZYNOCODES@users.noreply.github.com>
2022-11-25 16:03:40 +0100

    Second Commit

--- src/main/java/com/example/ihmproject/UserInterFace.java
+++ src/main/java/com/example/ihmproject/UserInterFace.java
@@ -11,7 +11,7 @@ public class UserInterFace extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(UserInterFace.class.getResource("UserInterFace.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 310, 240);
         stage.setScene(scene);
         stage.show();
     }
